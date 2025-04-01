5127873b78ca90200a4a8b9e82a191af4817d7a8
Tom Durrant <t.durrant@oceanum.science>
2025-04-01 16:57:54 +1100

    Added version

--- rompy_oceanum/__init__.py
+++ rompy_oceanum/__init__.py
@@ -4,10 +4,10 @@ rompy-oceanum: Oceanum Prax integration for rompy
 This package extends rompy to allow submitting model runs to Oceanum's Prax system.
 """
 
-from .prax import PraxClient, PraxResult
 from .extension import extension
 from .model_extension import OceanumModelRun
+from .prax import PraxClient, PraxResult
 
 __all__ = ["PraxClient", "PraxResult", "OceanumModelRun", "extension"]
 
-# No need to initialize anymore, OceanumModelRun is ready to use
+__version__ = "pre-alpha"
